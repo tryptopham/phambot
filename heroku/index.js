6477947883d7c69359bd67b64f1f7b8b77f40148
tryptopham <tryptopham@hotmail.com>
2016-09-11 03:21:59 -0700

    scripting image fix

--- heroku/index.js
+++ heroku/index.js
@@ -33,7 +33,7 @@ class BetterSmoochApiBot extends SmoochApiBot {
 }
 
 const name = 'SmoochBot';
-const avatarUrl = 'https://s.gravatar.com/avatar/f91b04087e0125153623a3778e819c0a?s=80';
+const avatarUrl = 'http://tryptopham.sodoto.net/img/icon.steve.jpg';
 const store = new SmoochApiStore({
     jwt
 });
